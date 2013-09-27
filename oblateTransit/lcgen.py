38cbaa341ec1f200acf3d9c46932c160c80bb443
chelseah <huangxpku@gmail.com>
2013-09-27 12:42:58 -0400

    add in a simple script for convertion between rotation rate and oblateness

--- oblateTransit/lcgen.py
+++ oblateTransit/lcgen.py
@@ -33,7 +33,7 @@ def gentran(time,period,epoch,q):
     return intran
 
 
-def trangen(time,mag,transit):
+def trangen(time,mag,transit,lcflag=False):
     '''functions to generate fake lightcurves'''
     rmean = math.sqrt(transit.dip)
     f = 0.1
@@ -53,13 +53,23 @@ def trangen(time,mag,transit):
     tranwidth=transit.q*transit.P/2.
     obl = Obl.Oblateness(req,rpol,alpha,sma,inc,u1,u2)
     totalFlux = np.pi*(1.0-u1/3-u2/6)
-    phase = (time-transit.epoch)/transit.P-((time-transit.epoch)/transit.P).astype(int)
     intran = gentran(time,transit.P,transit.epoch,transit.q)
     medianmag=np.median(mag[-intran])
     print rpol,medianmag
     stdmag=np.std(mag[-intran])
-    dflux = np.zeros(len(time[intran]))
-    obl.relativeFlux(phase[intran],dflux)
+    phase = (time-transit.epoch)/transit.P-((time-transit.epoch)/transit.P).astype(int)
+    if(lcflag):
+        cadence = 1./60./24.
+        nt = (int)((max(time)-min(time))/cadence)
+        fktime = min(time)+np.arange(nt)*cadence
+        fkintran = gentran(fktime,transit.P,transit.epoch,transit.q) 
+        fkphase = (fktime-transit.epoch)/transit.P-((fktime-transit.epoch)/transit.P).astype(int)
+        fkdflux = np.zeros(len(fktime[fkintran]))
+        obl.relativeFlux(fkphase[fkintran],fkdflux)
+        dflux = binlc(time[intran],fktime[intran],fkdflux)
+    else:
+        dflux = np.zeros(len(time[intran]))
+        obl.relativeFlux(phase[intran],dflux)
     z=sma*np.sqrt(np.sin(phase*2*np.pi)*np.sin(phase*2*np.pi)+np.cos(phase*2*np.pi)*np.cos(phase*2*np.pi)*np.cos(inc)*np.cos(inc))
     circularflux = occultquad(z,u1,u2,rpol)[0]
     index = np.cos(phase*2*np.pi)<0
@@ -81,10 +91,10 @@ def main():
         #infile='HAT-365-0001481.epdlc'
         #infile='K972_short_notran.txt'
         #infile='data/kplr006603043-2011024051157_slc.tab'
-        infile='data/kplr006603043-2011145075126_slc.tab'
-        #infile='data/kplr006603043.ltf'
+        #infile='data/kplr006603043-2011145075126_slc.tab'
+        infile='data/kplr006603043.ltf'
         coljd=1
-        colmag=4
+        colmag=7
         #colmag=12
         transit.P=110.3216229;  
         rpstar=0.08453;
@@ -94,7 +104,7 @@ def main():
         #transit.epoch=min(time)+random.random();
         transit.epoch=1030.36382
         mag=[];readcolumn(mag,colmag,infile);mag=np.array(mag)
-        fkmag=trangen(time,mag,transit)
+        fkmag=trangen(time,mag,transit,lcflag=True)
         outfile=os.path.splitext(infile)[0]+'.fktrap'
         fout=open(outfile,mode='w')
         for i in range(len(time)):
