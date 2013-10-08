05928aa9b991ee518ffae2ac4251eddcf3c0f49f
chelseah <huangxpku@gmail.com>
2013-10-07 21:22:29 -0400

    add in the configure file feature for this package

--- oblateTransit/HATlc.py
+++ oblateTransit/HATlc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import math
 from const import *
+import cfg_parse as cfgp
 class lightcurve():
     def __init__(self):
         self.name=''
@@ -21,19 +22,37 @@ class tran():
         self.alpha = 0.0
         self.u1 = -1
         self.u2 = -1
+        self.stdmag = -1
     def __str__(self):
         return '%13.6f %13.6f %6.4e %4.2e \n' % (self.P, self.epoch,self.dip,self.q)
     def readpara(self,cfgfile):
-        self.P = float(cfgp.ltf_parse(cfgfile,'period')) 
-        self.q = float(cfgp.ltf_parse(cfgfile,'q'))
-        self.qg = float(cfgp.ltf_parse(cfgfile,'qgress'))
-        self.dip = float(cfgp.ltf_parse(cfgfile,'rpstar'))**2.
-        self.epoch =float(cfgp.ltf_parse(cfgfile,'epoch'))
-        self.f =float(cfgp.ltf_parse(cfgfile,'qgress'))
-        self.inc =
-        self.alpha =
-        self.u1 = 
-        self.u2 = 
+        self.P = float(cfgp.tran_parse(cfgfile,'period'))
+        self.u1 = float(cfgp.tran_parse(cfgfile,'u1'))
+        self.u2 = float(cfgp.tran_parse(cfgfile,'u2'))
+        self.dip = float(cfgp.tran_parse(cfgfile,'rpstar'))**2.
+        self.epoch =float(cfgp.tran_parse(cfgfile,'epoch'))
+        Tdur = cfgp.tran_parse(cfgfile,'Tdur')
+        if not Tdur == '':
+            self.q = float(Tdur)/24./self.P 
+        else: 
+            self.q = self.calq() 
+            print 'Warning: the default q is computed with solar radii and inc=90.'
+        qg = cfgp.tran_parse(cfgfile,'qgress')
+        if not qg == '': 
+            self.qg = float(qg)
+        f =cfgp.tran_parse(cfgfile,'planetf')
+        if not f == '':
+            self.f =float(f)
+        inc = cfgp.tran_parse(cfgfile,'inc')
+        if not inc=='':
+            self.inc = float(inc)
+        alpha = cfgp.tran_parse(cfgfile,'alpha')
+        if not alpha=='':
+            self.alpha = float(alpha)
+        
+        stdmag = cfgp.tran_parse(cfgfile,'stdmag')
+        if not stdmag=='':
+            self.stdmag = float(stdmag)
     def calq(self,rstar=1,logg=4.5):
         if(self.dip<=0):
             raise ValueError('A real planet to calculate qexp should have positive depth')
--- oblateTransit/lcgen.py
+++ oblateTransit/lcgen.py
@@ -9,13 +9,14 @@ from HATlc import tran
 import random
 import oblateness as Obl
 from occultquad import occultquad
+#from Eastman_occultquad import occultquad
 import matplotlib
 from matplotlib import pyplot as plt
+import cmd_parse as cmdp
+import cfg_parse as cfgp
 def binlc(filltime,time,mag):
     cadence = 30./60./24.
     nt = len(filltime)
-    #nt = (int)((time[-1]-time[0])/cadence)
-    #filltime = cadence*np.arange(nt)+time[0]
     fillmag = np.zeros(nt)
     for i in xrange(nt):
         samples=mag[np.abs(time-filltime[i])<cadence/2.]
@@ -28,9 +29,7 @@ def gentran(time,period,epoch,q):
     ftime=(time-epoch-0.5*period)/period-((time-epoch-0.5*period)/period).astype(int)
     ind=ftime<0
     ftime[ind]+=1
-    #print min(ftime),max(ftime)
     intran=(ftime > (0.5-q/2.0))*(ftime < (0.5+q/2.0))
-    #print ftime[intran])
     return intran
 
 
@@ -42,69 +41,69 @@ def trangen(time,mag,transit,lcflag=False):
     alpha = transit.alpha*np.pi/180.
     u1 = transit.u1
     u2 = transit.u2
-    #u1 = 0.076; u2 = 0.034; 
     sma = 1./transit.q/np.pi
-    print sma
-    #inc = math.acos(b/sma)
-    #rmean = 0.1
-    #f = 0.098
-
+    #print sma,transit.P,transit.u1,transit.u2,transit.epoch,transit.q,transit.qg
     req = rmean/math.sqrt(1-f)
     rpol = math.sqrt(1-f)*rmean
-    #fkmag=sp.zeros(len(time))+mag
     ntran=int((max(time)-transit.epoch)/transit.P)-int((min(time)-transit.epoch)/transit.P)+1
     
-    #print ntran,transit.epoch,transit.q,max(time),u1,u2,sma,alpha*180./np.pi,inc*180./np.pi
-    #print rmean,req,rpol
        
     tranwidth=transit.q*transit.P/2.
     obl = Obl.Oblateness(req,rpol,alpha,sma,inc,u1,u2)
     totalFlux = np.pi*(1.0-u1/3-u2/6)
     intran = gentran(time,transit.P,transit.epoch,transit.q)
     medianmag=np.median(mag[-intran])
-    stdmag=np.std(mag[-intran])
-    stdmag = 1.e-4
+    if transit.stdmag==-1:
+        stdmag=np.std(mag[-intran])
+    else:
+        stdmag=transit.stdmag
     #print rpol,medianmag
     phase = (time-transit.epoch)/transit.P-((time-transit.epoch)/transit.P).astype(int)
+
     ind = phase>0.5
     phase[ind]-=1.0
-
     fkmag=medianmag+np.random.randn(len(time))*stdmag
     if(lcflag):
+        #initial short cadence data
         circularfluxmeanlc=medianmag+np.random.randn(len(time))*stdmag
         cadence = 1./60./24.
         nt = (int)((max(time)-min(time))/cadence)
         fktime = min(time)+np.arange(nt)*cadence
         fkmagsc=medianmag+np.random.randn(len(fktime))*stdmag
+        
+        #initial short cadence pahse
         fkintran = gentran(fktime,transit.P,transit.epoch,transit.q) 
-        fkphase = (fktime-transit.epoch)/transit.P-((fktime-transit.epoch)/transit.P).astype(int)
+        fkphase = abs((fktime-transit.epoch)/transit.P)-(abs((fktime-transit.epoch)/transit.P)).astype(int)
         ind = fkphase>0.5
         fkphase[ind]-=1.0
+        z=sma*np.sqrt(np.sin(fkphase*2*np.pi)*np.sin(fkphase*2*np.pi)+np.cos(fkphase*2*np.pi)*np.cos(fkphase*2*np.pi)*np.cos(inc)*np.cos(inc))
+        
+        #compute corrections
         fkdflux = np.zeros(len(fktime[fkintran]))
         obl.relativeFlux(fkphase[fkintran],fkdflux)
-        z=sma*np.sqrt(np.sin(fkphase*2*np.pi)*np.sin(fkphase*2*np.pi)+np.cos(fkphase*2*np.pi)*np.cos(fkphase*2*np.pi)*np.cos(inc)*np.cos(inc))
+        #fkdflux/=totalFlux
         circularfluxmean = occultquad(z,u1,u2,rmean)[0]
+        circularfluxmean = 1-(1-circularfluxmean)/(totalFlux/np.pi)
         circularflux = occultquad(z,u1,u2,rpol)[0]
+        circularflux = 1-(1-circularflux)/(totalFlux/np.pi)
         index = np.cos(fkphase*2*np.pi)<0
         circularflux[index]=1.0
         circularfluxmean[index]=1.0
+        
         fkmagsc[fkintran] = medianmag
-        fkmagsc[fkintran]+=1-(circularflux[fkintran]-fkdflux/totalFlux)
+        fkmagsc[fkintran]+=1-(circularflux[fkintran]-fkdflux)
         circularfluxmean=1-circularfluxmean+medianmag
         fkmag[intran] = binlc(time[intran],fktime[fkintran],fkmagsc[fkintran])
         residual = (fkmagsc-circularfluxmean)/1.e-6
         circularfluxmeanlc[intran] = binlc(time[intran],fktime[fkintran],circularfluxmean[fkintran]) + np.random.randn(len(time[intran]))*stdmag
         circularfluxmean = circularfluxmeanlc 
-        #plt.plot(fkphase[fkintran],(-circularfluxmean[fkintran]+circularflux[fkintran]-fkdflux/totalFlux)/1.e-6,'r') 
-        #plt.xlim([-0.01,0.01])
-        #plt.show()
-        #plt.plot(fktime,1-circularflux+medianmag,'r',time,mag) 
-        #plt.show()
-        #dflux = binlc(time[intran],fktime[fkintran],fkdflux)
+        plt.plot(fkphase[fkintran],residual[fkintran],'r') 
+        plt.xlim([-0.01,0.01])
+
     else:
         dflux = np.zeros(len(time[intran]))
         obl.relativeFlux(phase[intran],dflux)
-            
+        dflux/=totalFlux
         z=sma*np.sqrt(np.sin(phase*2*np.pi)*np.sin(phase*2*np.pi)+np.cos(phase*2*np.pi)*np.cos(phase*2*np.pi)*np.cos(inc)*np.cos(inc))
     
         circularflux = occultquad(z,u1,u2,rpol)[0]
@@ -112,16 +111,16 @@ def trangen(time,mag,transit,lcflag=False):
         index = np.cos(phase*2*np.pi)<0
         circularflux[index]=1.0
     
-        circularfluxmean = occultquad(z,u1,u2,rmean)[0]
+        circularfluxmean,check = occultquad(z,u1,u2,rmean)
         circularfluxmean[index]=1.0
-
-
+        
+        plt.plot(phase[intran],(circularfluxmean[intran]-circularflux[intran]+dflux)/1.e-6,'r')
+        plt.xlim([-0.01,0.01])
         fkmag[intran] = medianmag+np.random.randn(len(fkmag[intran]))*stdmag
-        fkmag[intran]+=1-(circularflux[intran]-dflux/totalFlux)
+        fkmag[intran]+=1-(circularflux[intran]-dflux)
         circularfluxmean=1-circularfluxmean+medianmag+np.random.randn(len(fkmag))*stdmag
 
     plt.plot(phase[intran],(-circularfluxmean[intran]+fkmag[intran])/1.e-6,'o',mec='b',mfc='None',ms=1.5,mew=1)
-    plt.plot(fkphase[fkintran],residual[fkintran],'r')
     plt.show()
     
     return [fkmag,circularfluxmean]
@@ -130,43 +129,33 @@ def trangen(time,mag,transit,lcflag=False):
 def main():
     lcfile=lc()
     transit=tran()
-    if(1):	
-        path='./'
-        os.chdir(path)
-        #infile='HAT-365-0001481.epdlc'
-        #infile='K972_short_notran.txt'
-        #infile='data/kplr006603043-2011024051157_slc.tab'
-        #infile='data/kplr006603043-2011145075126_slc.tab'
-        infile='data/kplr006603043.ltf'
-        coljd=1
-        colmag=7
-        #colmag=12
-        #replace with the parsing inside transit class
-        transit.P=110.3216229;  
-        rpstar=0.08453;
-        transit.dip=rpstar**2.
-        transit.q=transit.calq(rstar=2.5,logg=4.07)
-        print transit.q
-        transit.f = 0.1
-        transit.inc = 89.209
-        transit.alpha = 45.
-        transit.u1 = 0.242
-        transit.u2 = 0.289
-        #
-        time=[];readcolumn(time,coljd,infile);time=np.array(time)
-        #transit.epoch=min(time)+random.random();
-        transit.epoch=1030.36382
-        mag=[];readcolumn(mag,colmag,infile);mag=np.array(mag)
-        #fkmag,circularflux=trangen(time,mag,transit)
-        fkmag,circularflux=trangen(time,mag,transit,lcflag=True)
+    options = cmdp.ltf_parse()
+    infileflag = options.infileflag
+    outfileflag = options.outfileflag
+    inpathflag = options.inpathflag
+    noplot=options.noplot
+    cfgfile = options.cfg
+    uflag = options.uflag
+    lcflag = int(cfgp.File_parse(cfgfile,'lcflag'))
+    infile = cfgp.File_parse(cfgfile,'infile')
+    inpath = cfgp.File_parse(cfgfile,'inpath')
+    outfile = cfgp.File_parse(cfgfile,'outfile')
+    coljd= int(cfgp.File_parse(cfgfile,'coljd'))
+    colmag= int(cfgp.File_parse(cfgfile,'colmag'))
+    if not inpath=='':
+        os.chdir(inpath)
+    transit.readpara(cfgfile)
+    time=[];readcolumn(time,coljd,infile);time=np.array(time)
+    mag=[];readcolumn(mag,colmag,infile);mag=np.array(mag)
+    fkmag,circularflux=trangen(time,mag,transit,lcflag=lcflag)
+    if outfile=='':
         outfile=os.path.splitext(infile)[0]+'.fktrap'
-        fout=open(outfile,mode='w')
-        fout.write('#Generated by lcgen with the following paramters:\n')
-        fout.write('#Period = %f, rpstar=%f, Tdur= %f, inc=%f, alpha=%f, f=%f\n' % (transit.P,rpstar,transit.q*transit.P*24.,transit.inc,transit.alpha,transit.f))
-        for i in range(len(time)):
-            #print i,time[i],mag[i],fkmag[i],circularflux[i]
-            fout.write('%13.6f %13.6f %13.6f %13.6f\n' % (time[i],mag[i],fkmag[i],circularflux[i]))
-        fout.close()
+    fout=open(outfile,mode='w')
+    fout.write('#Generated by lcgen with the following paramters:\n')
+    fout.write('#Period = %f, rpstar=%f, Tdur= %f, inc=%f, alpha=%f, f=%f\n' % (transit.P,math.sqrt(transit.dip),transit.q*transit.P*24.,transit.inc,transit.alpha,transit.f))
+    for i in range(len(time)):
+        fout.write('%13.6f %13.6f %13.6f %13.6f\n' % (time[i],mag[i],fkmag[i],circularflux[i]))
+    fout.close()
     return
 
 
