fix numerical error problem for long cadence from oblatenessfast

oblateTransit/lcfromdata.py
@@ -46,7 +46,7 @@ def simple():
     #infile='kplr006603043-2011024051157_slc.tab'
     #infile='kplr006603043-2013011073258_llc.tab'
     #infile='kplr006603043-2012277125453_llc.tab'
-    #infile='kplr006603043-2012179063303_llc.pflt'
+    infile='kplr006603043-2012179063303_llc.pflt'
     #infile='kplr006603043-2012088054726_llc.pflt'
     #infile='kplr006603043-2012004120508_llc.pflt'
     #infile = 'kplr006603043-2011271113734_llc.pflt'
@@ -58,7 +58,7 @@ def simple():
     #infile = 'kplr006603043-2010078095331_llc.pflt'
     #infile = 'kplr006603043-2010174085026_llc.pflt'
     #infile = 'kplr006603043-2010265121752_llc.pflt'
-    infile = 'kplr006603043-2010355172524_llc.pflt'
+    #infile = 'kplr006603043-2010355172524_llc.pflt'
     outfile=''
     coljd=1
     colmag=2
@@ -76,6 +76,8 @@ def simple():
     u2 = 0.289
     f = 0.1
     alpha =45./180.*np.pi
+    #f = 0.058
+    #alpha =11.22/180.*np.pi
     #f = 0.0
     #alpha =0./180.*np.pi
     req = rmean/sqrt(1-f)

oblateTransit/test.py
@@ -10,6 +10,66 @@ import matplotlib
 from matplotlib import pyplot as plt
 import time
 
+def Scaling():
+    rmean = 0.1
+    period = 100.
+    u1 = 0
+    u2 = 0
+    #f = 0.1
+    #farr = (1+np.arange(5))/5.*0.2
+    #farr = (1+np.arange(10))/10.*0.2
+    farr = np.array([0.1])
+    alphaarr =(np.arange(50)/50.*180.-90)
+    #alphaarr = np.array([0])
+    sma = (period/365)**(2./3.)*1.5e13/7.e10
+    #inc = 89.709/180.*np.pi
+    b0arr = np.arange(51)/50.*0.8
+    #b0arr = np.array([0.1])
+    #b0 = 0.8
+    #b0 = sma*cos(inc)
+    #print sma,b0
+    Npoint = 1000
+    percent = 0.025
+    dflux = np.zeros(Npoint)
+    dfluxF = np.zeros(Npoint)
+    for k in xrange(len(b0arr)):
+        for i in xrange(len(alphaarr)):
+            for j in xrange(len(farr)):
+                f = farr[j]
+                alpha=alphaarr[i]/180.*np.pi
+                inc = np.arccos(b0arr[k]/sma)
+                b0 = b0arr[k]
+                req = rmean/sqrt(1-f)
+                rpol = sqrt(1-f)*rmean
+                oblf = OblF.Oblateness(req,rpol,alpha,sma,inc,u1,u2)
+                if(b0>(1+req)):
+                    print 'no transit happen'
+                    return
+                dphi = 2*percent/(Npoint-1)
+                totalFlux = np.pi*(1.0-u1/3-u2/6)
+                phi=-1*percent+dphi*np.arange(Npoint)
+                #call
+                oblf.relativeFlux(phi,dfluxF)
+                #print phi,dflux
+                z=sma*np.sqrt(np.sin(phi*2*np.pi)*np.sin(phi*2*np.pi)+np.cos(phi*2*np.pi)*np.cos(phi*2*np.pi)*cos(inc)*cos(inc));
+                #z = abs(np.sin(phi*np.pi))*sma#/abs(np.sin(inc))
+                #print '3',time.time(),time.clock()-start
+                start = time.clock()
+                circularflux = occultquad(z,u1,u2,rpol)[0]
+                index = np.cos(phi*2*np.pi)<0
+                circularflux[index]=1.0
+                circularfluxmean = occultquad(z,u1,u2,rmean)[0]
+                circularfluxmean[index]=1.0
+                residual = (-circularfluxmean+circularflux-dfluxF/totalFlux)/1.e-6
+                #plt.xlim([-0.01,0.01])
+                ##plt.plot(phi,circularfluxmean)
+                #plt.plot(phi,residual)
+                #plt.show()
+                print alphaarr[i],farr[j],b0arr[k],max(residual) 
+    return
+
+
+
 def main():
     #rmean = 0.154679
     rmean = 0.08453
@@ -84,4 +144,5 @@ def main():
     return
 
 if __name__=='__main__':
-    main()
+    #main()
+    Scaling()